b7fbd8795e4122908ce7fe7877754225a1cb3a13
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-04 20:49:28 -0500

    finish onProfileUpdate

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, boxes, isProfileOpen } = this.state;
+    const { isSignedIn, imageUrl, route, boxes, isProfileOpen, user } = this.state;
     return (
       <div className="App">
          <Particles className="particles"
@@ -139,7 +139,11 @@ class App extends Component {
           toggleModal={this.toggleModal} />
         { isProfileOpen &&
           <Modal>
-            <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal} />
+            <Profile
+              isProfileOpen={isProfileOpen}
+              toggleModal={this.toggleModal}
+              loadUser={this.loadUser}
+              user={user}/>
           </Modal>
         }
         { route === 'home'
