2ac7076693c4531eff66c036e6dfcc151be1f0de
HyoSung Bidol-Lee <hyosung11@me.com>
2020-01-05 16:26:12 -0500

    need to fix DropdownMenu styling

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -23,19 +23,19 @@ class ProfileIcon extends React.Component {
   render() {
     return (
       <div className="pa4 tc">
-        <Dropdown right isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
           <DropdownToggle
             tag="span"
             data-toggle="dropdown"
             aria-expanded={this.state.dropdownOpen}
           >
-              <img
-                src="http://tachyons.io/img/logo.jpg"
-                className="br-100 ba h3 w3 dib" alt="avatar" />
+            <img
+              src="http://tachyons.io/img/logo.jpg"
+              className="br-100 h3 w3 dib" alt="avatar" />
           </DropdownToggle>
           <DropdownMenu
             right
-            className="b--transparent shadow-5"
+            className='b--transparent shadow-5'
             style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
             <DropdownItem onClick={this.props.toggleModal}>View Profile</DropdownItem>
             <DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
